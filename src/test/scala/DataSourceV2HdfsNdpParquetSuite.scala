e4e2c3420d36825694c2342bc4c140764a7ce357
Robert Foley <robfoley972@gmail.com>
2021-08-05 16:48:43 -0400

    Added new unit tests to confirm validity of unit test via a test using Spark's CSV data source. Also added a new test for exercising our NDP parquet support.

---
+++ src/test/scala/DataSourceV2HdfsNdpParquetSuite.scala
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.datasource.test
+
+import java.io.{BufferedWriter, OutputStreamWriter}
+import java.net.URI
+import java.nio.charset.StandardCharsets
+
+import org.apache.commons.io.IOUtils
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.FileSystem
+import org.apache.hadoop.fs.FSDataOutputStream
+import org.apache.hadoop.fs.Path
+
+import org.apache.spark.sql.DataFrame
+
+/** Is a suite of testing which exercises the
+ *  V2 data source, but using the HDFS binary API,
+ *  and a parquet file
+ */
+class DataSourceV2HdfsNdpParquetSuite extends DataSourceV2Suite {
+
+  var host = "dikehdfs"
+  /** Returns the dataframe for the sample data
+   *  read in through the ndp data source.
+   */
+  override protected def df(): DataFrame = {
+    spark.read
+      .format("pushdown")
+      .option("format", "parquet")
+      .option("outputFormat", "binary")
+      .load(s"ndphdfs://${host}/unit-test-parquet/")
+  }
+  override protected def dfNoHeader(): DataFrame = {
+     spark.read
+      .format("com.github.datasource")
+      .option("format", "parquet")
+      .option("outputFormat", "binary")
+      .load(s"ndphdfs://${host}/unit-test-parquet/")
+  }
+}
